ae752c239c0b124c0d0ab507c5c67f356c8c80a0
tkdhanasekar <tkdhanasekar@gmail.com>
2023-06-08 06:45:43 +0530

    s3

---
+++ s3-vars-method/outputs.tf
@@ -0,0 +1,8 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my-s3-bucket.id
+}
+
+output "s3_bucket_region" {
+    value = aws_s3_bucket.my-s3-bucket.region
+}
+
---
+++ s3-vars-method/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "ap-south-1"
+}
+
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = "my-s3-bucket"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+
